5dc364a153a7533195f88cb75b585b4e33361876
sumit <sumitjadala17@gmail.com>
2026-01-07 00:29:20 +0530

    payment-service initial commit

---
+++ services/payment-service/src/test/java/com/sj/ecommerce/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.ecommerce.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
